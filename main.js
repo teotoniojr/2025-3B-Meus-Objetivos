69f4697682eca4aaa4d78f54def1183d21fa9f6a
Teotonio José de Souza Junior <102707827+teotoniojr@users.noreply.github.com>
2025-05-22 10:24:34 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 const botoes = document.querySelectorAll(".botao");
 const textos = document.querySelectorAll(".aba-conteudo");
-
 const contadores = document.querySelectorAll(".contador");
 
 const tempoObjetivo01 = new Date("2025-04-15T00:00:00");
@@ -11,9 +10,7 @@ const tempoObjetivo04 = new Date("2025-11-10T00:00:00");
 const tempos = [tempoObjetivo01, tempoObjetivo02, tempoObjetivo03, tempoObjetivo04];
 
 for (let i = 0; i < botoes.length; i++) {
-
     botoes[i].onclick = function () {
-
         for (let j = 0; j < botoes.length; j++) {
             botoes[j].classList.remove('ativo');
             textos[j].classList.remove('ativo');
@@ -28,6 +25,10 @@ function calculaTempo(tempoObjetivo) {
     let tempoAtual = new Date();
     let tempoFinal = tempoObjetivo - tempoAtual;
 
+    if (tempoFinal <= 0) {
+        return null;
+    }
+
     let segundos = Math.floor(tempoFinal / 1000);
     let minutos = Math.floor(segundos / 60);
     let horas = Math.floor(minutos / 60);
@@ -36,22 +37,26 @@ function calculaTempo(tempoObjetivo) {
     segundos %= 60;
     minutos %= 60;
     horas %= 24;
-    if (tempoFinal > 0) {
-        return dias + " Dias " + horas + " Horas " + minutos + " Minutos " + segundos + " Segundos "
-    } else {
-        return "Objetivo concluido"
-    }
+
+    return [dias, horas, minutos, segundos];
 }
 
 function atualizaCronometro() {
-    for (i = 0; i < contadores.length; i++) {
-        document.getElementById("dias"+i).textContent = calculaTempo(tempos[i])[0];
-        document.getElementById("horas"+i).textContent = calculaTempo(tempos[i])[1];
-        document.getElementById("min"+i).textContent = calculaTempo(tempos[i])[2];
-        document.getElementById("seg"+i).textContent = calculaTempo(tempos[i])[3] ;
+    for (let i = 0; i < contadores.length; i++) {
+        let tempo = calculaTempo(tempos[i]);
+        
+        if (tempo) {
+            document.getElementById("dias" + i).textContent = tempo[0];
+            document.getElementById("horas" + i).textContent = tempo[1];
+            document.getElementById("min" + i).textContent = tempo[2];
+            document.getElementById("seg" + i).textContent = tempo[3];
+        } else {
+            document.getElementById("dias" + i).textContent = "-";
+            document.getElementById("horas" + i).textContent = "-";
+            document.getElementById("min" + i).textContent = "-";
+            document.getElementById("seg" + i).textContent = "-";
+        }
     }
 }
-
-
 atualizaCronometro();
 setInterval(atualizaCronometro, 1000);
